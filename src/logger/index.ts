0e9fd13d173b72b581da7c931ccf06798831e7eb
RashingPro <rashingpro@yandex.ru>
2025-07-18 20:47:57 +0300

    feat: add log levels to logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,5 +1,12 @@
 import * as fs from "node:fs";
 
+export enum LogLevel {
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR
+}
+
 export default class Logger {
     constructor(outPath: string = "./latest.log") {
         if (!fs.existsSync(outPath)) {
@@ -33,7 +40,20 @@ export default class Logger {
         return `\x1b[33m[${formattedValues[0]}.${formattedValues[1]}.${formattedValues[2]} – ${formattedValues[3]}:${formattedValues[4]}:${formattedValues[5]} ${formattedValues[6]}ms]`;
     }
 
-    log(...data: any[]) {
+    private getLogLevelPrefix(logLevel: LogLevel) {
+        switch (logLevel) {
+            case LogLevel.DEBUG:
+                return "DEBUG";
+            case LogLevel.INFO:
+                return "INFO";
+            case LogLevel.WARN:
+                return "WARN";
+            case LogLevel.ERROR:
+                return "ERROR";
+        }
+    }
+
+    private _log(logLevel: LogLevel, ...data: any[]) {
         let result: string = "";
         data.forEach((item) => {
             if (result.length > 0) result += " ";
@@ -50,7 +70,42 @@ export default class Logger {
             }
         });
         result = this.getPrefix() + " \x1b[0m" + result;
-        console.log(result);
-        this.addToFile(result);
+
+        switch (logLevel) {
+            case LogLevel.DEBUG: {
+                console.debug(result);
+                break;
+            }
+            case LogLevel.INFO: {
+                console.log(result);
+                break;
+            }
+            case LogLevel.WARN: {
+                console.warn(result);
+                break;
+            }
+            case LogLevel.ERROR: {
+                console.error(result);
+                break;
+            }
+        }
+
+        this.addToFile(`[${this.getLogLevelPrefix(logLevel)}] ` + result);
+    }
+
+    debug(...data: any[]) {
+        this._log(LogLevel.DEBUG, ...data);
+    }
+
+    log(...data: any[]) {
+        this._log(LogLevel.INFO, ...data);
+    }
+
+    warn(...data: any[]) {
+        this._log(LogLevel.WARN, ...data);
+    }
+
+    error(...data: any[]) {
+        this._log(LogLevel.ERROR, ...data);
     }
 }
