2ed6e87e4931a94fd9571ff463c70df1000faafb
RashingPro <rashingpro@yandex.ru>
2025-07-18 20:47:35 +0300

    feat: add onFail to guard

--- src/utils/guard.ts
+++ src/utils/guard.ts
@@ -1,9 +1,16 @@
 import { Message } from "node-telegram-bot-api";
 
-export default function guard(checkerFn: (message: Message) => boolean, callbackFn: (message: Message) => void) {
+export default function guard(
+    checkerFn: (message: Message) => boolean,
+    onPass: (message: Message) => void,
+    onFail?: (message: Message) => void
+) {
     return (message: Message) => {
         const check = checkerFn(message);
-        if (!check) return;
-        callbackFn(message);
+        if (!check) {
+            if (onFail) {
+                onFail(message);
+            }
+        } else onPass(message);
     };
 }
