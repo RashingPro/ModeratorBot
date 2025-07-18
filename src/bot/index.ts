321dac4d2e84d65471cdb06a0473e1dbf57cd89d
RashingPro <rashingpro@yandex.ru>
2025-07-18 19:26:57 +0300

    feat: implement commands and guard, handleMessage(), handleCommand()

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,20 +1,36 @@
-import TelegramBot, {ConstructorOptions, Message} from "node-telegram-bot-api";
-import guard from "../utils/guard";
 import Logger from "../logger";
+import guard from "../utils/guard";
+import commands from "./commands";
+import TelegramBot, { ConstructorOptions, Message } from "node-telegram-bot-api";
 
 export interface BotOptions {
-    token: string,
-    logger: Logger,
-    groupId: number
+    token: string;
+    logger: Logger;
+    groupId: number;
 }
 
 export default class Bot extends TelegramBot {
     constructor(options: BotOptions & ConstructorOptions) {
         super(options.token, { polling: true });
-        this.on("message", guard((msg) => true, this.handleCommand));
+        this.on(
+            "message",
+            guard((msg) => {
+                const chatId = msg.chat.id
+                return chatId == options.groupId
+            }, (...args) => this.handleMessage(...args))
+        );
     }
 
-    handleCommand(message: Message) {
+    async handleMessage(message: Message) {
+        if (message.text?.startsWith("/"))
+            await this.handleCommand(message);
+    }
 
+    async handleCommand(message: Message) {
+        const commandName = message.text?.split(" ")[0].slice(1)
+        if (!commandName) return;
+        const command = commands.find((value) => value.name === commandName);
+        if (!command) return;
+        await command.handler(this, message);
     }
 }
