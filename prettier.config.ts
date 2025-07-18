cc4f9516bf9839e8dd835220e110b95983d6c3ca
RashingPro <rashingpro@yandex.ru>
2025-07-18 18:12:48 +0300

    build: implement ESlint and Prettier imports order plugin

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,6 +3,7 @@ import { type Config } from "prettier";
 const config: Config = {
     trailingComma: "none",
     endOfLine: "crlf",
-    tabWidth: 4
+    tabWidth: 4,
+    plugins: ["@trivago/prettier-plugin-sort-imports"]
 };
 export default config;
